e7d49863bf5c44649a7437704c549c1b4448da30
TiagoPerrino <tiagoperrino@gmail.com>
2021-04-21 13:21:31 +0200

    Segunda Versión de PERRO

--- perro.py
+++ perro.py
@@ -4,3 +4,35 @@ class Perro():
         self.edad = edad
         self.peso = peso
         
+    def ladrar(self):
+        if self.peso >= 8:
+            print("WOOF, WOOF")
+        else:
+            print("woofi, woofi")
+            
+    def __str__(self):
+        return "Perro: {}, Edad: {}, Peso: {}".format(self.nombre, self.edad, self.peso)
+    
+class PerroAsistencia(Perro):
+    def __init__(self, nombre, edad, peso, amo):
+        Perro.__init__(self, nombre, edad, peso)
+        self.amo = amo
+        self.__trabajando = False
+        
+    def __str__(self):
+        return "Perro de Asistencia de {}".format(self.amo)
+        
+    def pasear(self):
+        print("Yo {} ayudo a mi dueño {} a pasear".format(self.nombre, self.amo))
+        
+    def ladrar(self):
+        if self.__trabajando:
+            print("Shhh, no puedo ladrar mientras trabajo.")
+        else:
+            Perro.ladrar(self)
+            
+    def trabajando(self, valor=None):
+        if valor == None:
+            return self.__trabajando
+        else:
+            self.__trabajando = valor
